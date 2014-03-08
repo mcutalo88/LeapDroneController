b3f776f6278d19f337628ca37061c7eb24c9a4f5
Mike Cutalo <mcutalo88@gmail.com>
2014-03-08 17:31:31 -0500

    Added Altitude controller

--- LeapDroneController.js
+++ LeapDroneController.js
@@ -1,41 +1,66 @@
+'use strict'
+
 var Leap  = require('leapjs');
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
+var controller = new Leap.Controller();
 
 var isTakenOff = false;
+var droneIncreaseTmp = 0;
 
-var controller = new Leap.Controller()
+
 controller.on("frame", function(frame) {
-  // console.log("Frame: " + frame.id + " @ " + frame.timestamp);
-  // console.log(frame);
-  // console.log("Hands: " frame.hand.yaw())
-
-  if(isTakenOff == false)
-    if(frame.hands.length == 1){
-      console.log("Take off");
-      isTakenOff = true;
+  //Drone will take off once a single hand is in place,
+  //This will also diticate the altitude of the drone 
+  if(frame.hands.length == 1){
+  
+    if(isTakenOff == false){ 
+      console.log("Taking off.......");
       client.takeoff();
       client.stop();
+      isTakenOff = true;
     }
-
-  if(frame.hands.length == 2){
-    console.log("Turning Clockwise");
-    client.clockwise(0.5);
+    
+    var hand = frame.hands[0];
+    var position = hand.palmPosition;
+    var handPosition = position[1] / 1000;        
+    
+    if(position != 0){
+      //forevery 1000 position increase the drone will move up .1
+      if(droneIncreaseTmp < handPosition){
+        console.log("Increasing Height by : " + handPosition);
+        client.up(handPosition); //value 0 -1
+      }else{
+        console.log("Decrease Height by : " + handPosition);  
+        client.down(handPosition); //value 0 -1
+      }        
+    }
+    droneIncreaseTmp = handPosition; //Saved old value
   }
+    
+  //On Two fingers the drone will turn click wise
+  // if(frame.fingers.length == 2){
+  //   console.log("Turning Clockwise .......");
+  //   client.clockwise(0.1);
+  // }
 
+  /* Stop the Drone and land */
   if(frame.hands.length == 0){
-    console.log('NO HANDS !')
-    //client.stop();
-    isTakenOff = false;
-    client.land();
+    if(isTakenOff == true){
+      console.log('Landing Drone.........')
+      client.stop();    
+      client.land();
+      isTakenOff = false;  
+    }    
   }
 
   // for (var i in frame.handsMap) {
-  //         var hand = frame.handsMap[i];
-  //          console.log("Roll: " + hand.roll());
-  //          console.log("Pitch: " +hand.pitch()); 
-  //          console.log("Yaw: " +hand.yaw());
+  //   var hand = frame.handsMap[i];
+  //   console.log("Roll: " + hand.roll());
+  //   console.log("Pitch: " +hand.pitch()); 
+  //   console.log("Yaw: " +hand.yaw());
   // }
+
 });
 
 // var frameCount = 0;
@@ -56,6 +81,7 @@ controller.on('connect', function() {
     console.log("connect");
 });
 controller.on('disconnect', function() {
+    client.land();
     console.log("disconnect");
 });
 controller.on('focus', function() {
@@ -68,6 +94,7 @@ controller.on('deviceConnected', function() {
     console.log("deviceConnected");
 });
 controller.on('deviceDisconnected', function() {
+    client.land();
     console.log("deviceDisconnected");
 });
 
