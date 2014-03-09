fb692267fcadcf436c646aa64d2724ae55b8ceff
Mike Cutalo <mcutalo88@gmail.com>
2014-03-09 15:12:58 -0400

    Comments

--- LeapDroneController.js
+++ LeapDroneController.js
@@ -1,6 +1,6 @@
 'use strict'
 /*
-Copyright (C) 2014  Mike Cutalo
+Copyright (C) 2014  Michael Cutalo
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
